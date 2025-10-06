c78fae5ddb04068372ea45662c918de7f9cdb809
BisaiSantoshKumar-Quadrant <santosh.kumar@quadranttechnologies.com>
2025-10-06 13:01:26 +0530

    Delete Documents/ingestq-frontend-master/ingestq-frontend-master directory

--- Documents/ingestq-frontend-master/ingestq-frontend-master/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: false
-  },
-  pageExtensions: ["ts", "tsx", "js", "jsx"] // tell Next what counts as pages
-};
-
-module.exports = nextConfig;
